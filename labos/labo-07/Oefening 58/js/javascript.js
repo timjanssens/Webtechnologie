4d6449ad7db8f576aa60ce2156eca8a6087f8aaf
Tim Janssens <timjanssens81@gmail.com>
2019-11-16 09:19:50 +0100

    "Finish oef58"

--- labos/labo-07/Oefening 58/js/javascript.js
+++ labos/labo-07/Oefening 58/js/javascript.js
@@ -1,22 +1,5 @@
-//constructor for passengers
-function Passenger(name, nationality, age) {
-    this.name = name;
-    this.nationality = nationality;
-    this.age = age;
-}
-
-//make objects passenger
-let passengerOne = new Passenger("Tim", "Belgium", 38);
-let passengerTwo = new Passenger("Lisbeth", "Belgium", 37);
-
-//array maken van objecten
-let arrayOfPassengerOne = Object.entries(passengerOne);
-let arrayOfPassengerTwo = Object.entries(passengerTwo);
-//2 arrays samenvoegen
-let AllPassengers = [arrayOfPassengerOne , arrayOfPassengerTwo];
-
 //constructor for bus
-function Bus(bestemming, nummerplaat, zetelsTotaal, zetelsBezet, passengers) {
+function Bus(bestemming, nummerplaat, zetelsTotaal, zetelsBezet) {
     this.bestemming = bestemming;
     this.nummerplaat = nummerplaat;
     this.zetelsTotaal = zetelsTotaal;
@@ -24,22 +7,42 @@ function Bus(bestemming, nummerplaat, zetelsTotaal, zetelsBezet, passengers) {
     this.zetelsVrij = function(){
         return this.zetelsTotaal - this.zetelsBezet;
     }
-    this.passengers = passengers;
+    this.passengers = [];
 }
 
 
+//constructor for passengers
+function Passenger(name, nationality, age) {
+    this.name = name;
+    this.nationality = nationality;
+    this.age = age;
+}
+
+
+
 //make objects bus
-let busEen = new Bus("Barcelona", "1 ABC 123", 55, 43, AllPassengers);
+let busEen = new Bus("Barcelona", "1 ABC 123", 55, 43);
 let busTwee = new Bus("Boedapest", "2 QWE 123", 80, 50);
 
 
-let heading = document.getElementById("heading");
-heading.textContent = busEen.bestemming;
+function addPassenger (bus, name, nationality, age) {
+    bus.zetelsBezet++;
+    let passenger = new Passenger(name,nationality,age);
+    bus.passengers.push(passenger);
+}
+
+addPassenger(busEen,"Tim", "Belgium", 38);
+addPassenger(busEen, "lisbeth", "Belgium", 37);
+
+
+ let heading = document.getElementById("heading");
+ heading.textContent = busEen.bestemming;
 
 
 let paragraph = document.getElementById("paragraph");
-//lus  maken om de passagiers uit de array te halen en deze samen te voegen
-for (i = 0 ; i <= AllPassengers.length ; i++)
-paragraph.textContent += busEen.passengers[i][0][1] + ", ";
+//lus  maken om de passagiers uit het object te halen en deze samen te voegen
+for (i = 0 ; i < Object.keys(busEen.passengers).length; i++)
+paragraph.textContent += busEen.passengers[i].name + ", ";
+
 
 
